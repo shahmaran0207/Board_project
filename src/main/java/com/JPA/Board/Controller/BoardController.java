bbfed8a1496947d7d4da9dcd3d70d626bdb1ba7b
shahmaran0207 <shahmaran0207@naver.com>
2024-10-28 14:04:12 +0900

    전체 게시글 조회

--- src/main/java/com/JPA/Board/Controller/BoardController.java
+++ src/main/java/com/JPA/Board/Controller/BoardController.java
@@ -4,10 +4,10 @@ import com.JPA.Board.DTO.BoardDTO;
 import com.JPA.Board.Service.BoardService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -29,7 +29,16 @@ public class BoardController {
     }
 
     @GetMapping("/list")
-    public String list() {
+    public String findAll(Model model) {
+
+        List<BoardDTO> boardlist=bs.findAll();
+        model.addAttribute("boardList", boardlist);
         return "/board/list";
+    }
+
+    @GetMapping("/{id}")
+    public String findById(@PathVariable int id, Model model) {
+
+        return "/board/view";
     }
 }
--- src/main/java/com/JPA/Board/DTO/BoardDTO.java
+++ src/main/java/com/JPA/Board/DTO/BoardDTO.java
@@ -1,5 +1,6 @@
 package com.JPA.Board.DTO;
 
+import com.JPA.Board.Entity.BoardEntity;
 import lombok.*;
 import java.time.LocalDateTime;
 
@@ -19,4 +20,18 @@ public class BoardDTO {
     private LocalDateTime boardUpdatedTime;
 
     private int boardHits;
+
+    public static BoardDTO toBoardDTO(BoardEntity boardEntity){
+        BoardDTO boardDTO = new BoardDTO();
+
+        boardDTO.setId(boardEntity.getId());
+        boardDTO.setBoardWriter(boardEntity.getBoardWriter());
+        boardDTO.setBoardPass(boardEntity.getBoardPass());
+        boardDTO.setBoardTitle(boardEntity.getBoardTitle());
+        boardDTO.setBoardContents(boardEntity.getBoardContents());
+        boardDTO.setBoardCreatedTime(boardEntity.getCreatedTime());
+        boardDTO.setBoardUpdatedTime(boardEntity.getUpdatedTime());
+
+        return boardDTO;
+    }
 }
